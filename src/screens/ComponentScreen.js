06d2697b1b761036007c49da68317a189cdd3c79
Ganesh Balaji Radhakrishnan <ganesh.balaji@brighter.se>
2022-10-05 21:58:25 +0200

    first commit

---
+++ src/screens/ComponentScreen.js
@@ -0,0 +1,12 @@
+import React from "react"
+import {Text, StyleSheet} from "react-native"
+
+const ComponentScreen = () => <Text style={styles.text}>ComponentScreen</Text>
+
+const styles = StyleSheet.create({
+    text : {
+        fontSize: 50
+    }
+})
+
+export default ComponentScreen
