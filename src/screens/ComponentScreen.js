d984e47fa356bf5ae3a4cba383c8f49cab09d505
Ganesh Balaji Radhakrishnan <ganesh.balaji@brighter.se>
2022-10-12 15:02:37 +0200

    eaad

--- src/screens/ComponentScreen.js
+++ src/screens/ComponentScreen.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import { Text, StyleSheet } from 'react-native'
 
-function ComponentScreen() {
-  return <Text style={styles.text}>ComponentScreen</Text>
-}
+const ComponentScreen = () => <Text style={styles.text}>ComponentScreen</Text>
 
 const styles = StyleSheet.create({
   text: {
