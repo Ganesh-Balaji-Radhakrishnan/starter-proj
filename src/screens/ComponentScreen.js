74838a318a4e86da187e1c2e16f283b851606188
Ganesh Balaji Radhakrishnan <ganesh.balaji@brighter.se>
2022-10-12 14:46:34 +0200

    sdfas

--- src/screens/ComponentScreen.js
+++ src/screens/ComponentScreen.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
-function ComponentScreen() {
+/* function ComponentScreen() {
   // eslint-disable-next-line react/jsx-filename-extension, no-use-before-define
   return <Text style={styles.text}>ComponentScreen</Text>;
+} */
+
+function ComponentScreen() {
+  return <Text style={styles.text}>ComponentScreen</Text>
 }
 
 const styles = StyleSheet.create({
