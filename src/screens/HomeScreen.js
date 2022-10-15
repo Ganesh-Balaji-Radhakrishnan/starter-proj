d3baf72a01ccc357a494b9dfb89cddd87c8ed0d6
Ganesh Balaji Radhakrishnan <ganesh.balaji@brighter.se>
2022-10-15 14:00:52 +0200

    ad

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,3 @@
-/* eslint-disable react/destructuring-assignment */
-/* eslint-disable react/prop-types */
 import React from 'react'
 import { View, Text, StyleSheet, Button } from 'react-native'
 
@@ -11,18 +9,15 @@ const styles = StyleSheet.create({
   },
 })
 
-const HomeScreen = (props) => (
+const HomeScreen = ({ navigation }) => (
   <View>
     <Text style={styles.text}>Hi everyone</Text>
-    <Button
-      onPress={() => props.navigation.navigate('Components')}
-      title="go to components screen"
-    />
-    <Button onPress={() => props.navigation.navigate('List')} title="go to list screen" />
+    <Button onPress={() => navigation.navigate('Components')} title="go to components screen" />
+    <Button onPress={() => navigation.navigate('List')} title="go to list screen" />
   </View>
 )
 
 export default HomeScreen
 
 // test
-// test 2
+// test 3
