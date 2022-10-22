1c6fa7f07f2ac343004edcb46746b9edaf0f7445
Ganesh Balaji Radhakrishnan <ganesh.balaji@brighter.se>
2022-10-22 22:21:59 +0200

    added color screen template

---
+++ src/screens/ColorScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Button } from 'react-native'
+
+const ColorScreen = () => (
+  <View>
+    <Button title="Color Screen" />
+  </View>
+)
+
+export default ColorScreen
